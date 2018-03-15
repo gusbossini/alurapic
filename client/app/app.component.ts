61dec19f4e1f2baec291840bff8e4104f9bc8789
Gustavo Bossini <bossinigus@gmail.com>
2018-03-15 13:54:42 -0300

    Adicionando um painel para melhor visualização das fotos e títulos.

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id,
     selector: 'app',
     templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+
+    fotos: Object[] = [];
+
+    constructor(http: Http/*@Inject(Http) http*/) {
+        
+        http
+        .get('v1/fotos')
+        .map(res => res.json())
+        .subscribe(fotos => {this.fotos = fotos;}, erro => console.log(erro));
+    }
+ }
