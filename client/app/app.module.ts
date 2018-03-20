2b5d37970cc3c1f1b0ca533c7975f141ee8e9521
Gustavo Bossini <bossinigus@gmail.com>
2018-03-20 11:32:52 -0300

    Incluindo rotas para cadastro e listagem.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,10 +5,13 @@ import { FotoModule } from './foto/foto.module';
 import { HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { PainelModule } from './painel/painel.module';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { routing } from './app.routes';
 
 @NgModule({
-    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule ],
-    declarations: [ AppComponent ],
+    imports: [ BrowserModule, FotoModule, HttpModule, PainelModule, routing ],
+    declarations: [ AppComponent, CadastroComponent, ListagemComponent ],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
